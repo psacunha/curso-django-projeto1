631500f0b141c36510876234fa1db6ae40cca431
Paulo Sérgio Cunha <psacunha86@gmail.com>
2022-07-16 22:46:39 -0300

    Removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from recipes.views import home, contato, sobre
-
+from recipes.views import home
 
 urlpatterns = [
-    path('', home),  # Home
-    path('sobre/', sobre),  # /Sobre/
-    path('contato/', contato),  # /Contato/
+    path('', home),
 ]
