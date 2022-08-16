819b4df5151dba4f0d60968c5050cbd56a47776f
Paulo Sérgio dos Anjos Cunha <psacunha86@gmail.com>
2022-08-15 21:36:37 -0300

    Atualização Languagem e TimeZone

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
+
 from .models import Recipe
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -10,16 +12,19 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True,).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True,).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |',
     })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
 
     })
- 
